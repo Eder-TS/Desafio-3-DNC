73a3a69808aebd57bbe875aea34b48cdd54b7532
Eder-TS <steder20@yahoo.com.br>
2024-07-17 22:11:35 -0300

    Renderized cards in Project section

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,23 @@
+import projects from './projects'
+import Card from '../elements/Card'
+import styles from './Projects.module.css'
+
 export default function Projects(){
     return(
-        <>
-        </>
+        <section className={styles.projects}>
+            <h1>Projetos</h1>
+
+            <div className={styles.cards}>
+                {projects.map((project) => 
+                    <Card key={project.id} 
+                        site={project.site}
+                        src={project.src}
+                        alt={project.alt}
+                        title={project.title}
+                        description={project.description}
+                        repo={project.repo}
+                /> )}
+            </div>
+        </section>
     )
 }
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -7,47 +7,52 @@ const projects = [
         title: 'Clima e Logradouro',
         description: 'Projeto desenvolvido para uso interno de uma empresa onde os funionários podem pesquisar um logradouro a partir de um CEP e a tempretaura local a partir das coordenadas dadas.',
         repo: 'https://github.com/Eder-TS/Desafio2_DncWeather'
-    }/*,
-    {
-        id:0,
-        src:,
-        alt:,
-        title:,
-        description:,
-        repo:
     },
     {
-        id:0,
-        src:,
-        alt:,
-        title:,
-        description:,
-        repo:
+        id:1,
+        site:'https://desafioccxp22.netlify.app/',
+        src: '/ccxp.png',
+        alt: 'CCXP22',
+        title: 'Timer para evento',
+        description: 'Projeto criado usando um modelo no Figma. Aplicado HTML e CSS para o layout, Javascript para a contagem regressiva.',
+        repo: 'https://github.com/Eder-TS/Desafio-CCXP22'
     },
     {
-        id:0,
-        src:,
-        alt:,
-        title:,
-        description:,
-        repo:
+        id:2,
+        site: 'https://recrutadoramicheli.netlify.app/',
+        src: '/portfoliomi.png',
+        alt: 'Portfólio recrutadora',
+        title: 'Portfólio de uma recrutadora',
+        description: 'Portfólio criado com informações da cliente e baseado em modelo preestabelecido. Aplicado HTML e CSS, página estática.',
+        repo: 'https://github.com/Eder-TS/Portfolio-Mi'
     },
     {
-        id:0,
-        src:,
-        alt:,
-        title:,
-        description:,
-        repo:
+        id:3,
+        site: 'https://inscricaobootcampdnc.netlify.app',
+        src: '/bootcamp.png',
+        alt: 'Bootcamp DNC',
+        title: 'Página de inscrição para evento',
+        description: 'Exercício de criação de página. Foco na aplicação de CSS e responsividade.',
+        repo: 'https://github.com/Eder-TS/Bootcamp-DNC'
     },
     {
-        id:0,
-        src:,
-        alt:,
-        title:,
-        description:,
-        repo:
-    }*/
+        id:4,
+        site: 'https://dapper-pothos-7da56a.netlify.app/',
+        src: '/landpage.png',
+        alt: 'Landing page DNC',
+        title: 'Landing page',
+        description: 'Landing page para prospecto de novos alunos. No final há um formulário para inscrição. HTML, CSS e Javascript.',
+        repo: 'https://github.com/Eder-TS/PROJETO-LANDINGPAGE'
+    },
+    {
+        id:5,
+        site: 'https://minhatabuada-dnc.netlify.app/',
+        src: '/calc.png',
+        alt: 'Calculadora',
+        title: 'Calculadora de tabuada',
+        description: 'Exercício de manipulação do Document Object Model usando Javascript',
+        repo: 'https://github.com/Eder-TS/Tabuada'
+    }
 ];
 
 export default projects
